d4a79f43e898a2c5766750905b5863823d2cf4ad
Vadim <92179377+Zhuzhalica@users.noreply.github.com>
2024-11-26 20:39:13 +0500

    delete test app

--- ConsoleTestApp/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using BotTemplate;
-using Yandex.Cloud.Functions;
-
-var handler = new TelegramHandler();
-var request = "{ \"body\": \"123\" }";
-Context context = null;
-
-handler.FunctionHandler(request, context);
